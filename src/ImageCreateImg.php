d65b47e78ca2c3426ee484973ea90ceeb69cf1ac
火线兔 <silverhawk@foxmail.com>
2022-09-30 17:10:08 +0800

    Changes

---
+++ src/ImageCreateImg.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tobycroft\AossSdk;
+
+class ImageCreateImg
+{
+    public $type = "image";
+
+    public string $position = "mm";
+    public mixed $x = 0;
+    public mixed $y = 0;
+    public string $url = "";
+}
---
+++ src/ImageCreateText.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tobycroft\AossSdk;
+
+class ImageCreateText
+{
+    public $type = "text";
+
+    public string $position = "lt";
+    public string $text = "";
+    public int $size = 13;
+    public mixed $x = 0;
+    public mixed $y = 0;
+    public string $url = "";
+    public string $font_color = "000000";
+}
