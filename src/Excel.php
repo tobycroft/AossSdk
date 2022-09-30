e160d2a254186dce68a9fbc72680cdc60e859e3c
火线兔 <silverhawk@foxmail.com>
2022-09-30 16:47:04 +0800

    Changes

--- src/Excel.php
+++ src/Excel.php
@@ -6,11 +6,10 @@ class Excel extends Aoss
 {
     protected string $send_path = "/v1/excel/index";
 
-    public function __construct($token, $mode = "complete", $remote_url = "")
+    public function __construct($token, $remote_url = "")
     {
         $this->send_url = $remote_url;
         $this->token = $token;
-        $this->mode = $mode;
 
         if (empty($remote_url)) {
             $this->send_url = $this->remote_url;
