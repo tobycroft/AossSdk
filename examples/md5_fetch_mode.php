948ba8d0ce5702f0eaa742de878c2d0d9068603e
火线兔 <silverhawk@foxmail.com>
2022-10-09 15:03:12 +0800

    Changes

--- examples/md5_fetch_mode.php
+++ examples/md5_fetch_mode.php
@@ -5,7 +5,13 @@ use Tobycroft\AossSdk\Aoss;
 /*
  * your_token can be get by sending request email
  * complete mode can gave your the datas as much as possible with video/audio analyze
+ *
+ * this example gave you a way of send files
+ * 1.you can directly send the file to AOSS then retrieve the attach url finally send it to your own server.
+ * 2.you can let the front-end sending the file to AOSS then let FE gave you the MD5 hash code which returns by AOSS, and then use this function to retrieve the truly url in Back-End,
+ *   this can helps you to defend the XSS attack.
  */
+
 $Aoss = new Aoss("your_token", "complete");
 $md5_data = $Aoss->md5("md5 sign here");
 if (empty($md5_data->error)) {
