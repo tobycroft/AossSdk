23838b7ef0b90d6d798174a12b612f28e5fab8ee
火线兔 <silverhawk@foxmail.com>
2022-10-13 16:54:18 +0800

    Changes

--- src/ExcelCompleteRet.php
+++ src/ExcelCompleteRet.php
@@ -19,7 +19,7 @@ class ExcelCompleteRet
             if ($json["code"] == "0") {
                 $this->data = $json["data"];
                 foreach ($this->data as $key => $value) {
-                    $this->column[] = $key;
+                    $this->column[] = $value;
                 }
             } else {
                 $this->error = $json["data"];
