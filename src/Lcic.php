42f151cd68852470514eb55cb5f7403ebebf6c3e
火线兔 <silverhawk@foxmail.com>
2023-03-21 15:56:35 +0800

    Changes

--- src/Lcic.php
+++ src/Lcic.php
@@ -3,6 +3,7 @@
 namespace Tobycroft\AossSdk;
 
 use Tobycroft\AossSdk\Lcic\Ret\LcicRoomCreateRet;
+use Tobycroft\AossSdk\Lcic\Ret\LcicRoomModifyRet;
 use Tobycroft\AossSdk\Lcic\Ret\LcicUserAutoRet;
 use Tobycroft\AossSdk\Lcic\Url\LcicRouter;
 
@@ -44,16 +45,29 @@ class Lcic extends Aoss
         return $ret;
     }
 
-    public function RoomModify(string $RoomId, $TeacherId, $StartTime, $EndTime, $Name): LcicRoomCreateRet
+    public function RoomModify(string $RoomId, $TeacherId, $StartTime, $EndTime, $Name): LcicRoomModifyRet
     {
-        $ret = new LcicRoomCreateRet(
-            self::raw_post($this->remote_url . LcicRouter::lcic_room_create . $this->send_token . $this->token,
+        $ret = new LcicRoomModifyRet(
+            self::raw_post($this->remote_url . LcicRouter::lcic_room_modify . $this->send_token . $this->token,
                 [
                     'RoomId' => $RoomId,
                     'TeacherId' => $TeacherId,
                     'StartTime' => $StartTime,
                     'EndTime' => $EndTime,
                     'Name' => $Name,
+                ]
+            )
+        );
+        return $ret;
+    }
+
+    public function RoomUrl(string $OriginId, $TeacherId): LcicRoomCreateRet
+    {
+        $ret = new LcicRoomCreateRet(
+            self::raw_post($this->remote_url . LcicRouter::lcic_room_link . $this->send_token . $this->token,
+                [
+                    'OriginId' => $OriginId,
+                    'TeacherId' => $TeacherId,
                 ]
             )
         );
