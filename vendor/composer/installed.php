c786cc5b91986450b2cf2cf6f4687e80fac6e3e3
火线兔 <silverhawk@foxmail.com>
2023-03-21 16:57:31 +0800

    Changes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tobycroft/aoss-sdk',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4ea4b188efd6e8b644b7fc2bae1c78abbf0fc327',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tobycroft/aoss-sdk' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4ea4b188efd6e8b644b7fc2bae1c78abbf0fc327',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
