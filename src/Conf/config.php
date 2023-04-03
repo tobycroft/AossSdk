fbdb7b2e0cd1dc5feeff349f8d34c63ca79ac5da
火线兔 <silverhawk@foxmail.com>
2023-04-03 11:39:04 +0800

    Changes

---
+++ src/Conf/config.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tobycroft\AossSdk\Conf;
+
+const baseUrl = 'http://upload.familyeducation.org.cn:81';
+const baseUrls = 'https://upload.familyeducation.org.cn:444';
+const cdnUrl = 'http://aoss.familyeducation.org.cn';
+const cdnUrls = 'https://aoss.familyeducation.org.cn';
