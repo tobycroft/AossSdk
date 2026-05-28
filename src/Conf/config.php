d22268d4638dd64f5b6f0ddc3b5f7a40b8fb9dd4
火线兔 <tobycroft@users.noreply.github.com>
2026-05-28 17:10:42 +0800

    Changes

--- src/Conf/config.php
+++ src/Conf/config.php
@@ -2,7 +2,5 @@
 
 namespace Tobycroft\AossSdk\Conf;
 
-const baseUrl = 'http://upload.familyeducation.org.cn:81';
-const baseUrls = 'https://upload.familyeducation.org.cn:444';
-const cdnUrl = 'http://aoss.familyeducation.org.cn';
-const cdnUrls = 'https://aoss.familyeducation.org.cn';
+const baseUrl = 'http://upload.tuuz.cc:81';
+const baseUrls = 'https://upload.tuuz.cc:444';
